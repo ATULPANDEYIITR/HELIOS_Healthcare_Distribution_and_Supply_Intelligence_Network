723016d00490d0fa041963d150f79c984937b785
Atul Pandey <atulpandey.iitr@gmail.com>
2026-09-15 12:53:48 +0530

    Initialize project structure

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "HELIOS_Healthcare_Distribution_and_Supply_Intelligence_Network" << std::endl;
+    std::cout << "Project initialized successfully." << std::endl;
+
+    return 0;
+}
